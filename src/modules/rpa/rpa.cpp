b1cbc25cc62e2993202f169a5c9dcca2be20f2ba
Roman Kharitonov <rkharito@yandex.ru>
2019-12-06 03:10:01 +1000

    Read RenPy files offset and encryption key

--- src/modules/rpa/rpa.cpp
+++ src/modules/rpa/rpa.cpp
@@ -3,13 +3,17 @@
 
 #include "stdafx.h"
 #include "ModuleDef.h"
+#include "ModuleCRT.h"
 #include "modulecrt\Streams.h"
 
 #include <zlib.h>
 //#include <python3.7\Python.h>
 
-static const char* SIGNATURE = "RPA-3.0";
-static const char* SPACE = " ";
+#define FILE_SIGNATURE_RPA30 "RPA-3.0"
+#define WHITESPACE " "
+
+// TODO rename
+#define HANDLE_FILE_OPERATION(X) if (!X) return FALSE
 
 struct RenPyArchive
 {
@@ -19,14 +23,17 @@ struct RenPyArchive
 
 int MODULE_EXPORT OpenStorage(StorageOpenParams params, HANDLE* storage, StorageGeneralInfo* info)
 {
-    if (params.Data == nullptr) return SOR_INVALID_FILE;
+    if (!SignatureMatchOrNull(params.Data, params.DataSize, FILE_SIGNATURE_RPA30))
+        return SOR_INVALID_FILE;
 
     auto stream = CFileStream::Open(params.FilePath, true, false);
     if (stream == nullptr) return SOR_INVALID_FILE;
 
-    char signatureCandidate[sizeof(SIGNATURE) - 1];
-    stream->ReadBuffer(signatureCandidate, sizeof(signatureCandidate)); // TODO handle false
-    if (strncmp(signatureCandidate, SIGNATURE, strlen(SIGNATURE) != 0)) return SOR_INVALID_FILE;
+    char signatureCandidate[sizeof(FILE_SIGNATURE_RPA30) - 1];
+    if (!stream->ReadBuffer(signatureCandidate, sizeof(signatureCandidate)))
+        return SOR_INVALID_FILE;
+    if (!SignatureMatchOrNull(signatureCandidate, strlen(FILE_SIGNATURE_RPA30), FILE_SIGNATURE_RPA30))
+        return SOR_INVALID_FILE;
 
     auto archive = new RenPyArchive();
     archive->inputStream = stream;
@@ -56,18 +63,30 @@ int MODULE_EXPORT PrepareFiles(HANDLE storage)
     auto archive = (RenPyArchive*)storage;
     if (archive == nullptr) return FALSE;
 
-    char buffer[16];
+    char buffer[sizeof(int64_t)];
     char* dummy;
-    int64_t filesOffset;
-    int32_t encryptionKey;
-
-    archive->inputStream->Seek(strlen(SIGNATURE) + strlen(SPACE), STREAM_BEGIN); // TODO handle false
-    archive->inputStream->ReadBuffer(&buffer, sizeof(filesOffset)); // TODO handle false
-    filesOffset = std::strtoll(buffer, &dummy, 16);
-    archive->inputStream->Seek(strlen(SPACE), STREAM_CURRENT); // TODO handle false
+    HANDLE_FILE_OPERATION(archive->inputStream->Seek(strlen(FILE_SIGNATURE_RPA30) + strlen(WHITESPACE) + sizeof(int64_t), STREAM_BEGIN));
+    HANDLE_FILE_OPERATION(archive->inputStream->ReadBuffer(&buffer, sizeof(int64_t)));
+    int64_t filesOffset = std::strtoll(buffer, &dummy, 16);
+    HANDLE_FILE_OPERATION(archive->inputStream->Seek(strlen(WHITESPACE), STREAM_CURRENT));
     memset(buffer, 0, sizeof(buffer));
-    archive->inputStream->ReadBuffer(&buffer, sizeof(encryptionKey)); // TODO handle false
-    encryptionKey = std::strtol(buffer, &dummy, 16);
+    HANDLE_FILE_OPERATION(archive->inputStream->ReadBuffer(&buffer, sizeof(int64_t)));
+    int64_t encryptionKey = std::strtoll(buffer, &dummy, 16);
+
+    HANDLE_FILE_OPERATION(archive->inputStream->Seek(filesOffset, STREAM_BEGIN));
+
+    //z_stream zStream;
+    //zStream.next_in = (Bytef *)pvInBuffer;
+    //zStream.avail_in = (uInt)cbInBuffer;
+    //zStream.total_in = cbInBuffer;
+    //zStream.next_out = (Bytef *)pvOutBuffer;
+    //zStream.avail_out = *pcbOutBuffer;
+    //zStream.total_out = 0;
+    //zStream.zalloc = NULL;
+    //zStream.zfree = NULL;
+
+    //if (inflateInit(&compressedStream) != Z_OK)
+    //    return FALSE;
 
     return TRUE;
 }
